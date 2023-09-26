78bebcdc5ac738114dc0e0daa55f644a868f3f17
reidlo5135 <201840103@email.daelim.ac.kr>
2023-09-26 15:56:23 +0900

    feat : add load library

--- src/main/kotlin/net/wavem/lidar/Main.kt
+++ src/main/kotlin/net/wavem/lidar/Main.kt
@@ -1,1 +1,16 @@
 package net.wavem.lidar
+
+class LiDARNode {
+    companion object {
+        init {
+            System.loadLibrary("lidar_for_kotlin")
+        }
+    }
+
+    external fun nativeNodeRun()
+}
+
+fun main(args : Array<String>) {
+    val liDARNode : LiDARNode = LiDARNode()
+    liDARNode.nativeNodeRun()
+}
