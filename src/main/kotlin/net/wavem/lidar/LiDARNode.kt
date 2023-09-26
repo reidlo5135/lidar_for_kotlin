1fce29d108d09be50242a39fba8e21771f47f48e
reidlo5135 <201840103@email.daelim.ac.kr>
2023-09-26 20:50:04 +0900

    feat : add rcl spin logics

--- src/main/kotlin/net/wavem/lidar/LiDARNode.kt
+++ src/main/kotlin/net/wavem/lidar/LiDARNode.kt
@@ -3,14 +3,25 @@ package net.wavem.lidar
 class LiDARNode {
     companion object {
         init {
-            System.loadLibrary("liblidar_for_kotlin")
+            System.loadLibrary("lidar_for_kotlin")
         }
     }
 
     external fun nativeNodeRun()
+    external fun nativeRCLKotlinInit() : Long
+    external fun nativeCreateNode(rclKotlinInit : Long) : Long
+
+    external fun nativeSpin()
 }
 
 fun main(args : Array<String>) {
     val liDARNode : LiDARNode = LiDARNode()
-    liDARNode.nativeNodeRun()
+    val rclKotlinInit : Long = liDARNode.nativeRCLKotlinInit()
+
+    println("rclKotlinInit : $rclKotlinInit")
+
+    val rclKotlinNode : Long = liDARNode.nativeCreateNode(rclKotlinInit)
+    println("rclKotlinNode : $rclKotlinNode")
+
+    liDARNode.nativeSpin()
 }
